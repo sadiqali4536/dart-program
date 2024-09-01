0582cfc14770f236f71597e9d12c04c6e2a72b81
sadiqali4536 <sadiqali46221@gmail.com>
2024-09-01 22:29:46 +0530

    Add files via upload

---
+++ inverted_left_half_pyramid.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main(){
+  int i,j;
+  print("enter the rows:");
+  int n=int.parse(stdin.readLineSync()!);
+  for(i=0;i<n;i++){
+    for(j=0;j<=i;j++){
+      stdout.write(" ");
+    }
+    for(j=0;j<n-i;j++){
+      stdout.write("*");
+    }
+    stdout.write('\n');
+  }
+}
---
+++ inverted_right_half_pyramid.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+void main(){
+  int i,j;
+  print("enter a number: ");
+  int n=int.parse(stdin.readLineSync()!);
+  for(i=0;i<n;i++){
+    for(j=0;j<=n;j++){
+      if(i+j>=n){
+       stdout.write(" ");
+      }else{
+        stdout.write(" *");
+      }
+    }
+    stdout.write('\n');
+  }
+}
